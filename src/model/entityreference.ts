45181cec6e8958aeda8652d1a78cccc0629a78af
vedanta-krit.das <vedmalex@gmail.com>
2016-10-25 10:18:31 +0300

    update entity reference

--- src/model/entityreference.ts
+++ src/model/entityreference.ts
@@ -2,26 +2,57 @@ import { DEFAULT_ID_FIELDNAME, REF_PATTERN } from './definitions';
 import * as camelcase from 'camelcase';
 import { EntityReferenceInput } from './interfaces';
 
+
+
 /** Entityt reference implementation */
 export class EntityReference {
+  protected $obj: {
+    entity_: string;
+    field_: string;
+    entity: string;
+    field: string;
+  };
   /** the Entity that is referenced */
-  public entity: string;
+  public get entity(): string {
+    return this.$obj.entity || this.$obj.entity_;
+  }
+  public set entity(value: string) {
+    this.$obj.entity_ = value;
+  }
   /** the Identity field */
-  public field: string;
+  public get field(): string {
+    return this.$obj.field || this.$obj.field_ || DEFAULT_ID_FIELDNAME;
+  }
+
+  public set field(value: string) {
+    this.$obj.field_ = value;
+  }
 
   constructor(entity: string | EntityReferenceInput, field?: string) {
     if (typeof entity === 'string' && !field) {
       let res = entity.match(REF_PATTERN);
       if (res && res.length > 0) {
-        this.entity = res[1];
-        this.field = res[2] ? camelcase(res[2].trim()) : 'id';
+        this.$obj = {
+          entity: res[1],
+          entity_: res[1],
+          field: camelcase(res[2].trim()),
+          field_: camelcase(res[2].trim()) || DEFAULT_ID_FIELDNAME,
+        };
       }
     } else if (typeof entity === 'string') {
-      this.entity = entity;
-      this.field = field || 'id';
+      this.$obj = {
+        entity: entity,
+        entity_: entity,
+        field: field,
+        field_: field || DEFAULT_ID_FIELDNAME,
+      };
     } else if (entity instanceof Object) {
-      this.entity = entity.entity;
-      this.field = entity.field || 'id';
+      this.$obj = {
+        entity: entity.entity,
+        entity_: entity.entity,
+        field: entity.field,
+        field_: entity.field || DEFAULT_ID_FIELDNAME,
+      };
     }
   }
 
